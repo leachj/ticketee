07ec046ff081a7b4e96da41e80bcfb70d4e7ebc9
Jonathan Leach <jonathan@Jonathans-MacBook-Pro-2.local>
2016-02-19 14:42:44 +0000

    added validation on project name

--- ticketee/spec/features/creating_projects_spec.rb
+++ ticketee/spec/features/creating_projects_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "User can create new projects" do
-  scenario "with valid arrtibutes" do
+  
+  before do
     visit "/"
     
     click_link "New Project"
+  end
+  
+  scenario "with valid arrtibutes" do
     
     fill_in "Name", with: "Sublime Text 3"
     fill_in "Description", with: "A text editor"
@@ -14,5 +18,13 @@ RSpec.feature "User can create new projects" do
     project = Project.find_by(name: "Sublime Text 3")
     expect(page.current_url).to eq project_url(project)
     expect(page).to have_title "Projects - Ticketee"
+  end
+  
+  scenario "When providing invalid attributes" do
+    
+    click_button "Create Project"
+    
+    expect(page).to have_content "Project has not been created"
+    expect(page).to have_content "Name can't be blank"
   end
 end
